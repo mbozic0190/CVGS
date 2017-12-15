2faed9cbdfc6dd0a82c28ae4901bd6513ea44b00
Unknown <mirkobozic90@gmail.com>
2017-12-15 10:04:46 -0500

    iteration database
    added sql to alter database for iteration 3

---
+++ CVGS ALTER TABLES Iteration 3.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE shopping_cart(
+	shopping_cart_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	user_id INT FOREIGN KEY (user_id) REFERENCES users(user_id) NOT NULL,
+	platform_id INT FOREIGN KEY (platform_id) REFERENCES platforms(platform_id),
+	quantity INT);
